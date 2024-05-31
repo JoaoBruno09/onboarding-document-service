06a0792640b2777d5998377efa4587be3c11b841
João Rocha <1220256@isep.ipp.pt>
2024-05-31 12:35:26 +0100

    Developed Error events and its logic for each MCS

--- services/src/main/java/com/bank/onboarding/documentservice/services/DocumentService.java
+++ services/src/main/java/com/bank/onboarding/documentservice/services/DocumentService.java
@@ -1,8 +1,10 @@
 package com.bank.onboarding.documentservice.services;
 
 import com.bank.onboarding.commonslib.utils.kafka.CreateAccountEvent;
+import com.bank.onboarding.commonslib.utils.kafka.ErrorEvent;
 
 public interface DocumentService {
 
-    public void createDocumentForCreateAccountOperation(CreateAccountEvent createAccountEvent);
+    void createDocumentForCreateAccountOperation(CreateAccountEvent createAccountEvent);
+    void handleErrorEvent(ErrorEvent errorEvent);
 }
--- services/src/main/java/com/bank/onboarding/documentservice/services/impl/DocumentServiceImpl.java
+++ services/src/main/java/com/bank/onboarding/documentservice/services/impl/DocumentServiceImpl.java
@@ -7,13 +7,16 @@ import com.bank.onboarding.commonslib.persistence.services.CustomerRefRepoServic
 import com.bank.onboarding.commonslib.persistence.services.DocumentRepoService;
 import com.bank.onboarding.commonslib.utils.OnboardingUtils;
 import com.bank.onboarding.commonslib.utils.kafka.CreateAccountEvent;
-import com.bank.onboarding.commonslib.utils.mappers.AccountMapper;
+import com.bank.onboarding.commonslib.utils.kafka.ErrorEvent;
 import com.bank.onboarding.commonslib.utils.mappers.CustomerMapper;
+import com.bank.onboarding.commonslib.web.dtos.account.AccountRefDTO;
 import com.bank.onboarding.commonslib.web.dtos.account.CreateAccountRequestDTO;
 import com.bank.onboarding.commonslib.web.dtos.customer.CustomerDocumentsRequest;
+import com.bank.onboarding.commonslib.web.dtos.customer.CustomerRefDTO;
 import com.bank.onboarding.documentservice.services.DocumentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -21,6 +24,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.bank.onboarding.commonslib.persistence.constants.OnboardingConstants.DOCUMENT_TYPES_CREATE_ACCOUNT_REQUEST;
+import static com.bank.onboarding.commonslib.persistence.enums.OperationType.CREATE_ACCOUNT;
 
 @Slf4j
 @Service
@@ -31,14 +35,25 @@ public class DocumentServiceImpl implements DocumentService {
     private final AccountRefRepoService accountRefRepoService;
     private final CustomerRefRepoService customerRefRepoService;
     private final OnboardingUtils onboardingUtils;
+
+    @Value("${spring.kafka.producer.customer.topic-name}")
+    private String customerTopicName;
+
+    @Value("${spring.kafka.producer.account.topic-name}")
+    private String accountTopicName;
+
+    @Value("${spring.kafka.producer.intervention.topic-name}")
+    private String interventionTopicName;
+
     @Override
     public void createDocumentForCreateAccountOperation(CreateAccountEvent createAccountEvent) {
         List<CustomerDocumentsRequest> customerDocuments =  Optional.ofNullable(createAccountEvent.getCreateAccountRequestDTO()).map(CreateAccountRequestDTO::getCustomerDocuments)
                 .orElseThrow(() -> new OnboardingException("Não foram inseridos documentos")).stream().toList();
 
-        if(!customerDocuments.stream().allMatch(document -> DOCUMENT_TYPES_CREATE_ACCOUNT_REQUEST.contains(document.getDocumentType())))
-            //TODO -> Enviar evento para eliminar conta e retirar a conta do cliente
+        if(!customerDocuments.stream().allMatch(document -> DOCUMENT_TYPES_CREATE_ACCOUNT_REQUEST.contains(document.getDocumentType()))){
+            sendCreationEventErrors(createAccountEvent.getAccountRefDTO(), createAccountEvent.getCustomerRefDTO());
             throw new OnboardingException("O/s tipo/s de documento/s introduzido é inválido");
+        }
 
         customerDocuments.forEach(document -> {
             String documentType = document.getDocumentType();
@@ -47,12 +62,23 @@ public class DocumentServiceImpl implements DocumentService {
                     .documentType(documentType)
                     .documentBase64(document.getDocumentBase64())
                     .uploadedTime(LocalDateTime.now())
-                    .accountId(createAccountEvent.getAccountRefDTO().getAccountId())
                     .customerId(createAccountEvent.getCustomerRefDTO().getCustomerId())
                     .build());
         });
-
-        accountRefRepoService.saveAccountRefDB(AccountMapper.INSTANCE.toAccountRef(createAccountEvent.getAccountRefDTO()));
         customerRefRepoService.saveCustomerRefDB(CustomerMapper.INSTANCE.toCustomerRef(createAccountEvent.getCustomerRefDTO()));
+    }
+
+    private void sendCreationEventErrors(AccountRefDTO accountRefDTO, CustomerRefDTO customerRefDTO) {
+        onboardingUtils.sendErrorEvent(customerTopicName, accountRefDTO, customerRefDTO, CREATE_ACCOUNT);
+        onboardingUtils.sendErrorEvent(accountTopicName, accountRefDTO, customerRefDTO, CREATE_ACCOUNT);
+        onboardingUtils.sendErrorEvent(interventionTopicName, accountRefDTO, customerRefDTO, CREATE_ACCOUNT);
+        accountRefRepoService.deleteAccountById(accountRefDTO.getAccountId());
+    }
+
+    @Override
+    public void handleErrorEvent(ErrorEvent errorEvent) {
+        if(CREATE_ACCOUNT.equals(errorEvent.getOperationType())){
+            accountRefRepoService.deleteAccountById(errorEvent.getAccountRefDTO().getAccountId());
+        }
     }
 }
